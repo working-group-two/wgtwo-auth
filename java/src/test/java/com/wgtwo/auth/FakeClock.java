f2b7edb3e26c719b0fd67b646693af1fec08dbf1
Roy Sindre Norangshol <roysindre@wgtwo.com>
2023-08-21 18:11:41 +0200

    test: fail to refresh expired token
    Signed-off-by: Roy Sindre Norangshol <roysindre@wgtwo.com>

---
+++ java/src/test/java/com/wgtwo/auth/FakeClock.java
@@ -0,0 +1,40 @@
+package com.wgtwo.auth;
+
+import java.time.Clock;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+
+public class FakeClock extends Clock {
+    private Instant instant;
+    private ZoneId zoneId = ZoneOffset.UTC;
+
+    public FakeClock(Instant instant) {
+        this.instant = instant;
+    }
+
+    public static FakeClock forInstant(Instant instant) {
+        return new FakeClock(instant);
+    }
+
+    @Override
+    public ZoneId getZone() {
+        return zoneId;
+    }
+
+    @Override
+    public Clock withZone(ZoneId zone) {
+        this.zoneId = zone;
+        return this;
+    }
+
+    @Override
+    public Instant instant() {
+        return instant;
+    }
+
+    public synchronized void tick(Duration duration) {
+        instant = instant.plus(duration);
+    }
+}
--- java/src/test/kotlin/auth/ClientCredentialsTest.kt
+++ java/src/test/kotlin/auth/ClientCredentialsTest.kt
@@ -31,7 +31,7 @@ class ClientCredentialsTest {
 
         val clientId = "86b76cb5-caf3-4c1c-bdfe-8b3454f580b8"
         val clientSecret = "RzvcbQDfrkOb5ElvPuxA49oA8odBZfvj5MK1r2AdFuV99EGvL4aJvARUg637p3QqqgrU6gyG"
-        val wgtwoAuth = WgtwoAuth.builder(clientId,clientSecret)
+        val wgtwoAuth = WgtwoAuth.builder(clientId, clientSecret)
             .oauthServer("http://127.0.0.1:" + mockServer.localPort)
             .build()
 
@@ -49,15 +49,15 @@ class ClientCredentialsTest {
                 .withPath("/oauth2/token")
                 .withHeader(
                     "Authorization",
-                    "Basic ODZiNzZjYjUtY2FmMy00YzFjLWJkZmUtOGIzNDU0ZjU4MGI4OlJ6dmNiUURmcmtPYjVFbHZQdXhBNDlvQThvZEJaZnZqNU1LMXIyQWRGdVY5OUVHdkw0YUp2QVJVZzYzN3AzUXFxZ3JVNmd5Rw=="
+                    "Basic ODZiNzZjYjUtY2FmMy00YzFjLWJkZmUtOGIzNDU0ZjU4MGI4OlJ6dmNiUURmcmtPYjVFbHZQdXhBNDlvQThvZEJaZnZqNU1LMXIyQWRGdVY5OUVHdkw0YUp2QVJVZzYzN3AzUXFxZ3JVNmd5Rw==",
                 )
                 .withBody("scope=subscription.handset_details%3Aread&grant_type=client_credentials"),
-            Times.exactly(1)
+            Times.exactly(1),
         ).respond(
             HttpResponse.response()
                 .withStatusCode(200)
                 .withBody(javaClass.getResource("/access-token-with-id-token.json")!!.readText())
-                .withDelay(Delay.milliseconds(250))
+                .withDelay(Delay.milliseconds(250)),
         )
     }
 }
